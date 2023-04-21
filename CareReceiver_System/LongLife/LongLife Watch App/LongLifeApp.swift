39cc8e9c199452ac25b729a0b37a27b9bd5a1f21
Céline Madeleine Aldenhoven <celine.aldenhoven@tum.de>
2023-04-21 13:05:33 -0400

    CareReceiver App

---
+++ CareReceiver_System/LongLife/LongLife Watch App/LongLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongLifeApp.swift
+//  LongLife Watch App
+//
+//  Created by Céline Aldenhoven on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct LongLife_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareReceiver_System/LongLife/LongLife/LongLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongLifeApp.swift
+//  LongLife
+//
+//  Created by Céline Aldenhoven on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct LongLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
