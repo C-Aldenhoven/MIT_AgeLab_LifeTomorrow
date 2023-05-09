ac095b48029e7b15bc9d08d4cf93546f15503109
Céline Madeleine Aldenhoven <celine.aldenhoven@tum.de>
2023-05-09 16:11:42 -0400

    consent form structure

--- LifeTomorrow/LifeTomorrow/HomeViewController.swift
+++ LifeTomorrow/LifeTomorrow/HomeViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseAuth
+import ResearchKit
 
 class HomeViewController: UIViewController {
 
@@ -45,6 +46,18 @@ class HomeViewController: UIViewController {
         NSLayoutConstraint.activate(constraints)
         
         self.navigationItem.titleView = topTitleLabel
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "ConsentSigned") {
+            // Consent form has been signed, proceed as normal
+        } else {
+            // Consent form has not been signed. Show consent form (perhaps using performSegueWithIdentifier)
+            let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
+            taskViewController.delegate = self
+            present(taskViewController, animated: true, completion: nil)
+            UserDefaults.standard.set(true, forKey: "TermsAccepted")
+        }
     }
     
     // MARK: UI-Elements
@@ -146,5 +159,14 @@ class HomeViewController: UIViewController {
     @objc private func carePlanButtonAction() {
         navigationController?.pushViewController(CarePlanViewController(), animated: true)
     }
+}
+
+extension HomeViewController : ORKTaskViewControllerDelegate {
+    // Your view controller now implements ORKTaskViewControllerDelegate by handling task results in taskViewController:didFinishWithReason:error:. These results could be in the form of a signed consent document, survey responses, or sensor data. For now, you are simply dismissing the task’s view controller when the task finishes
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        //Handle results with taskViewController.result
+        taskViewController.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
